aaebca51145628ac9a332956b400afabe8c82ae2
Ingrid Munhoz <ingridmunuoz@MacBook-Air-de-Ingrid.local>
2025-05-12 17:17:11 -0300

    Praticando logica

---
+++ LogicaDaProgramacao/IniciandoComJavaScript/Desafios/desafioListas.js
@@ -0,0 +1,17 @@
+// Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+// Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin','Python'];
+// Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramacao);
+// Crie uma lista com 3 séries e exiba no console apenas o primeiro elemento.
+let listaSeries = ['The Last of Us', 'Succesion', 'Breaking Bad']
+console.log(listaSeries[0]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+let listaDeNomes = ['Ingrid', 'William', 'Tito']
+console.log(listaDeNomes[1]);
+
+// Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaDeNomes[2]);
